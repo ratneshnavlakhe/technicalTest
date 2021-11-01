f0795ad28d5ba6ba03016003497bb21d75e2c470
Ratnesh Navlakhe <ratnesh061288@gmail.com>
2021-11-01 13:41:45 +0530

    Refactor api headers to use interceptors

--- app/src/main/java/com/example/android/technicaltest/UserEndpoint.kt
+++ app/src/main/java/com/example/android/technicaltest/UserEndpoint.kt
@@ -4,16 +4,13 @@ import com.example.android.technicaltest.model.User
 import com.example.android.technicaltest.model.UserListResponse
 import io.reactivex.Single
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Path
 
 interface UserEndpoint {
 
-    @Headers("app-id: 617a643fd02e388d62fef08a")
     @GET("data/v1/user?limit=100")
     fun getUserList(): Single<UserListResponse>
 
-    @Headers("app-id: 617a643fd02e388d62fef08a")
     @GET("data/v1/user/{user_id}")
     fun getUser(@Path("user_id") userId: String): Single<User>
 }
--- app/src/main/java/com/example/android/technicaltest/network/NetworkFactory.kt
+++ app/src/main/java/com/example/android/technicaltest/network/NetworkFactory.kt
@@ -2,6 +2,8 @@ package com.example.android.technicaltest.network
 
 import com.example.android.technicaltest.di.NetworkDependency
 import com.google.gson.Gson
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,6 +14,19 @@ class NetworkFactory {
             .addConverterFactory(GsonConverterFactory.create(Gson()))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .baseUrl(dependency.getBaseUrl())
+            .client(getInterceptor())
             .build()
+    }
+
+    private fun getInterceptor(): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            addInterceptor(
+                Interceptor { chain ->
+                    val builder = chain.request().newBuilder()
+                    builder.header("app-id", "617a643fd02e388d62fef08a")
+                    return@Interceptor chain.proceed(builder.build())
+                }
+            )
+        }.build()
     }
 }
